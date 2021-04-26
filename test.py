5e1b226f4b1e883147696c093993d12e10dfb6eb
kierensharma <kieren.sharma@gmail.com>
2021-04-26 13:41:43 +0100

    Some rough template matching

--- test.py
+++ test.py
@@ -9,10 +9,11 @@ from itertools import chain
 image_path = '/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/micrographs_final/FoilHole_24671225_Data_24671816_24671817_20181025_2332-80407.mrc'
 image = get_mrc_image(image_path)
 imgplot = plt.imshow(image)
-
-line_image, line_image_s, long_lines = find_actin_ll(image)
-imgplot = plt.imshow(line_image)
 plt.show()
+
+# line_image, line_image_s, long_lines = find_actin_ll(image)
+# imgplot = plt.imshow(line_image)
+# plt.show()
 
 labels, data = get_trp_coords('/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/coordinates/FoilHole_24671225_Data_24671816_24671817_20181025_2332-80407.star')
 
@@ -20,10 +21,10 @@ for coords in chain(data):
     x_coord = coords[0]
     y_coord = coords[1]
 
-    # plt.scatter(x_coord, y_coord, c='white', marker="1")
+    plt.scatter(x_coord, y_coord, c='white', marker="1")
 
-    troponin_img = grab_troponin(image, x_coord, y_coord, grab_rad=300)
+    troponin_img = grab_troponin(image, x_coord, y_coord, grab_rad=150)
     imgplot = plt.imshow(troponin_img)
-    # plt.show()
+    plt.show()
 
 # plt.show()
