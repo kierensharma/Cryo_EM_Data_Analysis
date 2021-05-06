79d4e217d68a0a2f4d93f1eee6147b47d0b03241
kierensharma <kieren.sharma@gmail.com>
2021-05-06 12:33:38 +0100

    Change os.listdir to glob

--- export_troponin_imgs.py
+++ export_troponin_imgs.py
@@ -1,4 +1,4 @@
-import os
+import glob
 import cv2
 from troponin import get_mrc_image
 from troponin import find_actin_ll
@@ -6,26 +6,30 @@ from troponin import get_trp_coords
 from troponin import grab_troponin
 import matplotlib.pyplot as plt
 from itertools import chain 
+from tqdm import tqdm
 
 def get_trop_imgs():
-    num = 0
-    mrc_direct = os.listdir('/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/micrographs_final')
-    mrc_direct.sort()
-    star_direct = os.listdir('/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/coordinates')
-    star_direct.sort()
-    for mrc_file, star_file in zip(mrc_direct, star_direct):
-        mrc_path = '/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/micrographs_final/' + mrc_file[2:]
-        image = get_mrc_image(mrc_path)
-        star_path = '/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/coordinates/' + star_file[2:]
-        labels, data = get_trp_coords(star_path)
+    mrc_files = []
+    for file in glob.glob("/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/micrographs_final/*.mrc"):
+        mrc_files.append(file)
+    star_files = []
+    for file in glob.glob("/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/coordinates/*.star"):
+        star_files.append(file)
+
+    mrc_files.sort()
+    star_files.sort()
+
+    for mrc_file, star_file in zip(mrc_files, star_files):
+        image = get_mrc_image(mrc_file)
+        labels, data = get_trp_coords(star_file)
 
         for coords in data:
             x_coord = coords[0]
             y_coord = coords[1]
 
             troponin_img = grab_troponin(image, x_coord, y_coord, grab_rad=150)
-            trop_img_path = '/Volumes/KierenSSD/University/Troponin_Images/trop_' + str(num) + '.png'
-            plt.imsave(trop_img_path, troponin_img) 
+            trop_img_path = '/Volumes/KierenSSD/University/New_Troponin_Images/trop_' + str(num) + '.png'
+            plt.imsave(trop_img_path, troponin_img)
 
             num += 1
 
--- image_data_generation.py
+++ image_data_generation.py
@@ -1,34 +1,49 @@
-import os
+import glob
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from tqdm import tqdm
 
-def data_generator(directory):
+def troponin_generator():
     datagen = ImageDataGenerator(
-            rotation_range=40,
+            rotation_range=90,
             width_shift_range=0.2,
             height_shift_range=0.2,
-            shear_range=0.2,
-            zoom_range=0.2,
+            shear_range=0,
+            zoom_range=0,
             horizontal_flip=True,
-            fill_mode='nearest')
+            fill_mode='reflect')
 
-    troponin_directory = os.listdir(directory)
-    # troponin_directory.sort()
+    troponin_files = []
+    for file in glob.glob("/Volumes/KierenSSD/University/Troponin_Images/*.png"):
+        troponin_files.append(file)
 
-    for troponin in troponin_directory:
-        img_path = '/Volumes/KierenSSD/University/Troponin_Images/' + troponin
-        img = load_img(img_path)
+    for troponin_file in tqdm(troponin_files):
+        img = load_img(troponin_file)
         x = img_to_array(img)
         x = x.reshape((1,) + x.shape) 
 
         i = 0
         for batch in datagen.flow(x, batch_size=1,
-                                save_to_dir='/Volumes/KierenSSD/University/Generated_troponin_dataset', save_prefix='troponin', save_format='jpeg'):
+                                save_to_dir='/Volumes/KierenSSD/University/Generated_troponin_dataset_new', save_prefix='troponin', save_format='jpeg'):
             i += 1
             if i > 20:
                 break  
 
+def not_troponin_generator():
+    mrc_files = []
+    for file in glob.glob("/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/micrographs_final/*.mrc"):
+        mrc_files.append(file)
+    star_files = []
+    for file in glob.glob("/Volumes/KierenSSD/University/Thinfil_micrographs_and_coords/coordinates/*.star"):
+        star_files.append(file)
+
+    mrc_files.sort()
+    star_files.sort()
+
+    
+
 def main():
-    data_generator('/Volumes/KierenSSD/University/Troponin_Images/')
+    troponin_generator()
+    # not_troponin_generator()
 
 if __name__ == '__main__':
     main()
